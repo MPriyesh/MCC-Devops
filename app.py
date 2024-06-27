6ea2d979ff6dee1511010eb34ceefad7533af109
MPriyesh <manepriyesh777@gmail.com>
2024-06-27 06:47:26 +0000

    Implemented new endpoint for feature/new-feature

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ def read_root():
 @app.get("/about")
 def about():
     return {"msg": "About Us"}
+
+
+@app.get("/contact")
+def contact():
+    return {"email": "manepriyesh777@gmail.com", "phone": "8767565401"}
+
